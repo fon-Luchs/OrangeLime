d51935ba4b25f1eec2f0ffa1083b5756cbd6ff4d
fon-Luchs <lvo123ua@gmail.com>
2017-11-30 23:57:37 +0200

    Animation Update

--- client/component/Login/LoginAninm/AnimationComponent.jsx
+++ client/component/Login/LoginAninm/AnimationComponent.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import style from './AnimationComponent.css';
 
 const Animation = (props) => (
-        <div className={style.AnimationComponent}> {props.tst} </div>
+    <div class={style.main}>
+        <div class={style.anim}></div>
+        <div class={style.anim2}></div>
+        <div class={style.anim3}></div>
+        <div class={style.anim4}></div>
+        <div class={style.anim5}></div>
+        <div class={style.anim6}></div>
+    </div>
 );
 
 export default Animation
